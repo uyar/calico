ca1126a407294f068d6cfc02ded1518d8920febf
H. Turgut Uyar <uyar@itu.edu.tr>
2018-06-18 15:54:54 +0300

    Update Python dependency to 3.6

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'Intended Audience :: Education',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Education :: Testing',
         'Topic :: Utilities'
     ],
