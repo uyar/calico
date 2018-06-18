d400d1e76eefe6cf2f62543fd414b7d0a5886d7f
H. Turgut Uyar <uyar@itu.edu.tr>
2018-06-18 16:55:32 +0300

    Apply black style to tests

--- tests/test_calico.py
+++ tests/test_calico.py
@@ -2,28 +2,28 @@ from calico import run_script
 
 
 def test_script_expect_eof_should_be_ok():
-    result = run_script('true', [('expect', '_EOF_', None)])
+    result = run_script("true", [("expect", "_EOF_", None)])
     assert result == (0, [])
 
 
 def test_script_expect_output_should_be_ok():
-    result = run_script('echo 1', [('expect', '1', None), ('expect', '_EOF_', None)])
+    result = run_script("echo 1", [("expect", "1", None), ("expect", "_EOF_", None)])
     assert result == (0, [])
 
 
 def test_script_expect_with_timeout_should_be_ok():
-    result = run_script('sleep 1', [('expect', '_EOF_', 2)])
+    result = run_script("sleep 1", [("expect", "_EOF_", 2)])
     assert result == (0, [])
 
 
 def test_script_expect_with_exceeded_timeout_should_report_error():
-    result = run_script('sleep 2', [('expect', '_EOF_', 1)])
-    assert result == (None, ['Timeout exceeded.'])
+    result = run_script("sleep 2", [("expect", "_EOF_", 1)])
+    assert result == (None, ["Timeout exceeded."])
 
 
 def test_script_expect_with_unmatched_output_should_report_error():
-    result = run_script('true', [('expect', '1', None)])
-    assert result == (0, ['Expected output not received.'])
+    result = run_script("true", [("expect", "1", None)])
+    assert result == (0, ["Expected output not received."])
 
 
 # def test_script_expect_with_extra_output_should_report_error():
@@ -32,11 +32,12 @@ def test_script_expect_with_unmatched_output_should_report_error():
 
 
 def test_script_send_input_should_be_ok():
-    result = run_script('bash -c "read x && echo $x"',
-                        [('send', '1', None), ('expect', '1', None)])
+    result = run_script(
+        'bash -c "read x && echo $x"', [("send", "1", None), ("expect", "1", None)]
+    )
     assert result == (0, [])
 
 
 def test_timeout_should_kill_infinite_program():
-    result = run_script('yes', [('expect', '_EOF_', 1)])
-    assert result == (None, ['Timeout exceeded.'])
+    result = run_script("yes", [("expect", "_EOF_", 1)])
+    assert result == (None, ["Timeout exceeded."])
